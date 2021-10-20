edae3b9f84ed797838dd94292bbf8bb143e5fb8f
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2021-10-19 20:42:55 -0500

    experimenting with MUI

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import { deepOrange, deepPurple } from "@mui/material/colors";
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
-const Sidebar = () => {
+export default function SimplePaper() {
   return (
-    <div className="Sidebar">
-      <h1>Sidebar Compnonent</h1>
-    </div>
+    <Paper>
+      <Stack
+        direction="column"
+        divider={<Divider orientation="horizontal" flexItem />}
+        spacing={2}
+      >
+        <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>
+    </Paper>
   );
-};
-
-export default Sidebar;
+}
--- client/src/pages/main/Main.tsx
+++ client/src/pages/main/Main.tsx
@@ -1,13 +1,21 @@
 // This will be the main index file to display the main application
 // so have the sidebar and navbar componetns here and based on state, change whether to show home feed or account settings
 
-import React from "react";
 import Navbar from "../../components/Navbar";
+import Sidebar from "../../components/Sidebar";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+
 const Main = () => {
   return (
     <div className="Main">
       <Navbar />
-      <h1>This is the primary main page</h1>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Sidebar />
+        <h1>This is the primary main page</h1>
+      </Container>
     </div>
   );
 };
